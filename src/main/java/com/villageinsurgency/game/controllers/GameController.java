179c0ed74d8b68c9b0d86507d9b20876470cf445
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2024-11-16 20:19:30 -0800

    added all endpoints

--- src/main/java/com/villageinsurgency/game/controllers/GameController.java
+++ src/main/java/com/villageinsurgency/game/controllers/GameController.java
@@ -2,10 +2,7 @@ package com.villageinsurgency.game.controllers;
 
 import com.villageinsurgency.game.services.GameService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GameController {
@@ -15,29 +12,24 @@ public class GameController {
         this.gameService = gameService;
     }
 
-    @PutMapping("/units/create")
-    public ResponseEntity<String> createUnit(@RequestParam int gameKey, @RequestParam String type, @RequestParam String user) {
-        return gameService.createUnit(gameKey, type, user);
+    @GetMapping("/games")
+    public ResponseEntity<String> getGame(@RequestParam int id) {
+        String gameJson = gameService.getGameObjectJsonById(id);
+        return ResponseEntity.ok(gameJson);
     }
-
     @PostMapping("/games/create")
     public ResponseEntity<String> createNewGame(@RequestParam String p1) {
+        gameService.createGame(p1);
         return gameService.createGame(p1);
     }
-
     @PutMapping("/games/join")
     public ResponseEntity<String> joinGame(@RequestParam int gameKey, @RequestParam String p2){
         return gameService.joinGame(gameKey, p2);
     }
-
-//    @GetMapping("/Games")
-//    public ResponseEntity<String> getGame(@RequestParam int gameKey) throws InterruptedException, ExecutionException {
-//        String gameJson = gameService.getGameObjectJsonById(gameKey);
-//        return ResponseEntity.ok(gameJson);
-//    }
-
-
-
+    @PutMapping("/units/create")
+    public ResponseEntity<String> createUnit(@RequestParam int gameKey, @RequestParam String type, @RequestParam String user) {
+        return gameService.createUnit(gameKey, type, user);
+    }
 }
 
 
