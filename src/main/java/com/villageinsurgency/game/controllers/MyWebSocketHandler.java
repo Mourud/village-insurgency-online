9ad9079a7215155635537f03d75474bf66aa0ddc
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2024-11-17 00:49:24 -0800

    added websockets messaging as POC

--- src/main/java/com/villageinsurgency/game/controllers/MyWebSocketHandler.java
+++ src/main/java/com/villageinsurgency/game/controllers/MyWebSocketHandler.java
@@ -1,25 +1,42 @@
 package com.villageinsurgency.game.controllers;
 
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-import org.springframework.web.socket.TextMessage;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MyWebSocketHandler extends TextWebSocketHandler {
+    private final Set<WebSocketSession> sessions = Collections.newSetFromMap(new ConcurrentHashMap<>());
+
+
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
         System.out.println("New connection: " + session.getId());
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         System.out.println("Received: " + message.getPayload());
+
         // Echo back the message
-        session.sendMessage(new TextMessage("Server: " + message.getPayload()));
+        // Broadcast the message to all connected clients
+//        for (WebSocketSession activeSession : sessions) {
+//            System.out.println("Session: " + activeSession.getId());
+//            if (activeSession.isOpen()) {
+//                activeSession.sendMessage(new TextMessage("Broadcast: " + message.getPayload()));
+//            }
+//        }
+//        session.sendMessage(new TextMessage("Server: " + message.getPayload()));
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) throws Exception {
         System.out.println("Connection closed: " + session.getId());
+        sessions.remove(session);
     }
 }
